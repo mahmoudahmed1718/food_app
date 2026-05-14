49be8bbe3f78d7f3a0897d97deccb9926227499d
mahmoudahmed1718 <mahmoudahmedhsn@gmail.com>
2026-05-14 19:32:58 +0300

    add navagtion function in splash

--- lib/features/splash/views/splash_view.dart
+++ lib/features/splash/views/splash_view.dart
@@ -1,32 +1,61 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_app/core/constants/app_colors.dart';
 import 'package:food_app/core/constants/assets.dart';
-import 'package:gap/gap.dart';
-class SplashView extends StatelessWidget {
+
+class SplashView extends StatefulWidget {
   static const String routeName = '/splash';
 
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      if (!mounted) return;
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-             Gap(100),
-              SvgPicture.asset(Assets.assetsImagesSvgLogo ,colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),)
-               ,    Spacer(),
-            FittedBox(child: Image.asset(Assets.assetsImagesPngSplashImage))
-             
-            ],
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: size.height * 0.12),
+
+            SvgPicture.asset(
+              Assets.assetsImagesSvgLogo,
+              colorFilter: const ColorFilter.mode(
+                Colors.white,
+                BlendMode.srcIn,
+              ),
+            ),
+
+            const Spacer(),
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 0),
+              child: Image.asset(
+                Assets.assetsImagesPngSplashImage,
+                width: size.width,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-  
 }
