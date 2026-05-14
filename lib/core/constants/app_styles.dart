23cc32a1b00a98c4ce0dffd6dd9c4c0d2472ac46
mahmoudahmed1718 <mahmoudahmedhsn@gmail.com>
2026-05-14 18:40:24 +0300

    add styels fonts

---
+++ lib/core/constants/app_styles.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  // Roboto Regular 16
+  static const TextStyle regular16 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    height: 1.35,
+    letterSpacing: 0,
+  );
+
+  // Roboto SemiBold 16
+  static const TextStyle semiBold16 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 1.35,
+    letterSpacing: 0,
+  );
+
+  // Roboto Medium 12
+  static const TextStyle medium12 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    height: 1,
+    letterSpacing: 0,
+  );
+
+  // Inter SemiBold 18
+  static const TextStyle interSemiBold18 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 1.35,
+    letterSpacing: 0,
+  );
+
+  // Roboto SemiBold 18
+  static const TextStyle semiBold18 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 1.35,
+    letterSpacing: 0,
+  );
+
+  // Roboto SemiBold 18 Center
+  static const TextStyle semiBold18Center = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 1.54,
+    letterSpacing: 0,
+  );
+
+  // Reem Kufi Ink 32
+  static const TextStyle reemKufi32 = TextStyle(
+    fontFamily: 'ReemKufiInk',
+    fontWeight: FontWeight.w400,
+    fontSize: 32,
+    height: 1,
+    letterSpacing: 0,
+  );
+}
